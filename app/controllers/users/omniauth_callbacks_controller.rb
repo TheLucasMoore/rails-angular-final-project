eea58bc922b84af1803e17a27c044a555d6d9a06
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-06-04 00:40:49 -0500

    pull and persist associated data from hash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -7,13 +7,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
 
     if @user.persisted?
-      # then do the rest of the stuff
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Spotify") if is_navigational_format?
     else
       session["devise.spotify_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
+
+    top = spotify_user.top_artists
+    Artist.parse_from_user(@user, top)
    end
 
   def failure
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,22 @@
 class Artist < ActiveRecord::Base
   has_many :artist_users
   has_many :users, through: :artist_users
+
+  def self.parse_from_user(user, array)
+    array.each do |artist|
+      newbie = Artist.find_or_create_by(name: artist.name)
+
+      newbie.name = artist.name
+      newbie.image = artist.images[0]["url"]
+      newbie.popularity = artist.popularity
+      newbie.link = artist.href
+      newbie.uri = artist.uri
+      newbie.followers = artist.followers["total"]
+
+      user.artists << newbie
+
+      newbie.save
+      user.save
+    end
+  end
 end
