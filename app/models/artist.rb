f3a4139e7e523765a9602664308d78b99e9f154c
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-06-16 10:44:15 -0500

    refactor

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -8,19 +8,21 @@ class Artist < ActiveRecord::Base
   # passed in from Users::OmniauthCallbacksController
   def self.parse_from_user(user, array)
     array.each do |artist|
-      newbie = Artist.find_or_create_by(name: artist.name)
+      new_artist = Artist.find_or_create_by(name: artist.name)
 
-      newbie.name = artist.name
-      newbie.image = artist.images[0]["url"]
-      newbie.popularity = artist.popularity
-      newbie.link = artist.external_urls["spotify"]
-      newbie.uri = artist.uri
-      newbie.followers = artist.followers["total"]
+      new_artist.name = artist.name
+      new_artist.image = artist.images[0]["url"]
+      new_artist.popularity = artist.popularity
+      new_artist.link = artist.external_urls["spotify"]
+      new_artist.uri = artist.uri
+      new_artist.followers = artist.followers["total"]
 
-      user.artists << newbie unless user.artists.include?(newbie)
-
-      newbie.save
-      user.save
+      new_artist.artist_users.build(user: user)
+      # user.artists << new_artist unless user.artists.include?(new_artist)
+      # # user.artist_users.build(:artist => new_artist)
+      # new_artist.save # When this happens
+      # user.save
+      new_artist.save
     end
   end
 end
