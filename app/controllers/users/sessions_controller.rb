d5ead905930b89d9e7867330d28eddf6520b9087
TheLucasMoore <lucasedwardmoore@gmail.com>
2016-06-21 08:21:46 -0600

    Fixing Heroku errors

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,20 +1,20 @@
 class Users::SessionsController < Devise::SessionsController
-before_action :configure_sign_in_params, only: [:create]
+# before_action :configure_sign_in_params, only: [:create]
 
-  GET /resource/sign_in
-  def new
-    super
-  end
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
 
-  POST /resource/sign_in
-  def create
-    super
-  end
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
 
-  DELETE /resource/sign_out
-  def destroy
-    super
-  end
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
 
   # protected
 
